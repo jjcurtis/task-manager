762887de10d94a4fba464492ded16b18c3145d7f
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-03 14:24:41 -0500

    Create Layout wrapper function around App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './tailwind.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './tailwind.css';
+import Layout from './components/ui/Layout.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Layout>
+      <App />
+    </Layout>
+  </React.StrictMode>
+);
