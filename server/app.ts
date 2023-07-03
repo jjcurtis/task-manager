127b952c6b14de45679f6e15d6a92f4b4a312647
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-03 14:22:42 -0500

    Add cors support

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import { config } from 'dotenv';
 import todosRouter from './routes/todos'
 
@@ -6,6 +7,7 @@ config()
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/api/todos', todosRouter)
 
 app.get('/', (req, res) => {
