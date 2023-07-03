0bc4a4fdb783fd828ac2954475cd83f61b6f0ecf
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-03 14:25:14 -0500

    Create Header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-slate-400 text-4xl p-4 shadow-white shadow-md">
+      <h1 className="font-semibold">Todo List</h1>
+    </header>
+  );
+}
