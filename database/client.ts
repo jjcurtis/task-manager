c8455bf9da8d3c892d45c8bfc5e23af143114bee
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-03 14:23:27 -0500

    Move client into readTodos function

--- database/client.ts
+++ database/client.ts
@@ -2,9 +2,9 @@ import { Client } from 'pg';
 import { config } from 'dotenv';
 config();
 
-const client = new Client(process.env.DATABASE_URL);
 
 export async function readTodos() {
+  const client = new Client(process.env.DATABASE_URL);
   await client.connect();
   try {
     const todos = (
