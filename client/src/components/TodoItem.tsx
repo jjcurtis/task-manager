80426d9c251b261bdd049549f4dbb8079abe3d6b
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:24:56 -0500

    Remove ITodo interface from file

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,21 +1,24 @@
-import formatDate from "../functions/formatDate"
-import getTime from "../functions/getTime"
+import { ITodo } from '../interfaces/ITodo';
+import formatDate from '../functions/formatDate';
+import getTime from '../functions/getTime';
+import Card from './ui/Card';
 
-export interface ITodo {
-  id: number,
-  task: string,
-  due: string,
-  completed: boolean
-}
-
-export default function TodoItem ({...todo}: ITodo) {
+export default function TodoItem({ ...todo }: ITodo) {
   return (
-    <article className="p-4 text-lg border-slate-400 bg-slate-200 border-2">
+    <Card>
       <div className="flex gap-2">
-        <input type="checkbox" defaultChecked={todo.completed} />
-        <h2>{todo.task}</h2>
+        <input
+          id={`checkbox_id ${todo.id}`}
+          type="checkbox"
+          defaultChecked={todo.completed}
+        />
+        <h2 className="text-2xl font-medium">
+          {todo.task}
+        </h2>
       </div>
-      <h4>{formatDate(todo.due)} at {getTime(todo.due)}</h4>
-    </article>
-  )
+      <h4>
+        Due {formatDate(todo.due)} at {getTime(todo.deadline)}
+      </h4>
+    </Card>
+  );
 }
