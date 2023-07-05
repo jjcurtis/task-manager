e7d1141e02f9bd26313ba0c0f4420e0493fef4c1
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:25:48 -0500

    Adjust imports/minor tweaks

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react"
-import TodoItem, { ITodo } from "./TodoItem";
+import { useEffect, useState } from 'react';
+import TodoItem from './TodoItem';
+import { ITodo } from '../interfaces/ITodo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState<ITodo[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/api/todos')
-    .then(res => res.json())
-    .then(payload => setTodos(payload))
-  }, [todos])
+      .then(res => res.json())
+      .then(payload => setTodos(payload));
+  }, [todos]);
 
   return (
-    <main className="grid md:grid-cols-2 lg:grid-cols-3 p-8">
+    <>
       {todos.map(todo => {
-        return <TodoItem key={todo.id} {...todo} />
+        return <TodoItem key={todo.id} {...todo} />;
       })}
-    </main>
-  )
+    </>
+  );
 }
