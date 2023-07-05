358965a655f21ae30a40dd1980223f8f9a45f4bd
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:22:06 -0500

    Update function to match new SQL schema

--- client/src/functions/getTime.ts
+++ client/src/functions/getTime.ts
@@ -1,9 +1,9 @@
 export default function getTime(date: string) {
-  let hour: string = date.slice(11, 13);
-  const minute: string = date.slice(14, 16)
+  let hour: string = date.slice(0, 2);
+  const minute: string = date.slice(3, 5)
   const cycle: string = hour.charAt(0) === '0' ? 'AM' : 'PM'
 
-  if (cycle === 'PM') {
+  if (cycle === 'PM' && hour !== '12') {
     hour = `${parseInt(hour) - 12}`
   }
 
