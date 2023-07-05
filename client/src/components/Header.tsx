f24cf934a9814d98b1f9f9bbde0c56146a694c63
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:28:04 -0500

    Move Menu into Header and create toggle state

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,28 @@
+import { useContext, useState } from 'react';
+import { TaskContext } from '../App';
+import Menu from './Menu';
+
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const setShowNewTask = useContext(TaskContext)
+
+  function handleClick() {
+    setShowNewTask!(showNewTask => !showNewTask)
+  }
+
   return (
-    <header className="bg-slate-400 text-4xl p-4 shadow-white shadow-md">
+    <header className="flex justify-between items-center bg-slate-400 text-4xl px-8 py-4 shadow-white shadow-md">
       <h1 className="font-semibold">Todo List</h1>
+      {showMenu && (
+        <nav>
+          <ul className="flex gap-8 text-2xl">
+            <li className='hover:text-slate-50 transition-colors'>
+              <button onClick={handleClick}>Create New Task</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+      <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
     </header>
   );
 }
