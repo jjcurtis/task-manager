25ad158c039d70075c9b07520386880a44c0305d
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:28:25 -0500

    Remove Layout component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App.js';
 import './tailwind.css';
-import Layout from './components/ui/Layout.tsx';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <Layout>
       <App />
-    </Layout>
   </React.StrictMode>
 );
