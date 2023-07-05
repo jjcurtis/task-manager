261c27cb41189fb578fbab0464248dd65747eb6e
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-04 23:30:27 -0500

    Add context with a useState setter to toggle grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
-import Header from './components/Header';
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+} from 'react';
 import TodoList from './components/TodoList';
+import NewItem from './components/NewItem';
+import Grid from './components/ui/Grid';
+import Header from './components/Header';
+
+export const TaskContext = createContext<null | Dispatch<
+  SetStateAction<boolean>
+>>(null);
 
 function App() {
+  const [showNewTask, setShowNewTask] = useState(false);
+
   return (
     <>
-      <Header />
-      <TodoList />
+      <TaskContext.Provider value={setShowNewTask}>
+        <Header />
+        {showNewTask ? (
+          <NewItem />
+        ) : (
+          <Grid>
+            <TodoList />
+          </Grid>
+        )}
+      </TaskContext.Provider>
     </>
   );
 }
