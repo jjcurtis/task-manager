2fa1506a1eb635f7f6da8151035b14c89a1d6a4d
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-05 19:54:17 -0500

    Create an API endpoint for updating tasks

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { readTodos, postTodo, deleteTodo } from '../../database/client'
+import { readTodos, postTodo, deleteTodo, updateTodoTask } from '../../database/client'
 const router = Router();
 
 router.get('/', async (req, res) => {
@@ -17,6 +17,12 @@ router.delete('/', async (req, res) => {
   await deleteTodo(req.body);
   const todos = await readTodos();
   res.json(todos)
+})
+
+router.put('/', async (req, res) => {
+  await updateTodoTask(req.body);
+  const todos = await readTodos();
+  res.json(todos);
 })
 
 export default router;
