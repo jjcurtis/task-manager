3a28850063906359624f98f42c5dfaaa1b635c96
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-07-05 22:50:04 -0500

    Clarify Date / Prettier formatting

--- client/src/components/todo/TodoDeadline.tsx
+++ client/src/components/todo/TodoDeadline.tsx
@@ -15,50 +15,65 @@ export default function TodoDeadline({ todo }: Props) {
 
   function handleClick(e: MouseEvent<HTMLButtonElement>) {
     if (updating === true) {
-      const [year, month, day] = date.split('-')
-      console.log(year, month, day)
+      const [year, month, day] = date.split('-');
+
+      const due = new Date(
+        parseInt(year),
+        parseInt(month) - 1,
+        parseInt(day)
+      );
+
       fetch('http://localhost:3000/api/todos/deadline', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ...todo, due: new Date(+(year), +(month) - 1, +(day)), deadline }),
+        body: JSON.stringify({ ...todo, due, deadline }),
       });
     }
-    setUpdating(updating => !updating)
+    setUpdating(updating => !updating);
   }
 
   function handleDate(e: ChangeEvent<HTMLInputElement>) {
-    setDate(e.currentTarget.value)
+    setDate(e.currentTarget.value);
   }
 
-  function handleDeadline(e: ChangeEvent<HTMLInputElement>) {
-    setDeadline(e.currentTarget.value)
+  function handleDeadline(
+    e: ChangeEvent<HTMLInputElement>
+  ) {
+    setDeadline(e.currentTarget.value);
   }
 
   return (
     <div className="flex gap-2 items-start">
       Due{' '}
       <span className="font-semibold">
-        {
-        updating ?
-        <input type="date" name="date" id="date" value={date}
-        onChange={handleDate}
-        />
-        :
-        formatDate(date
+        {updating ? (
+          <input
+            type="date"
+            name="date"
+            id="date"
+            value={date}
+            onChange={handleDate}
+          />
+        ) : (
+          formatDate(date)
         )}
       </span>{' '}
       at{' '}
       <span className="font-semibold">
-        {
-        updating ?
-        <input type='time' value={deadline}
-        onChange={handleDeadline}
-        />
-        :
-        getTime(deadline)
-        }
+        {updating ? (
+          <input
+            type="time"
+            value={deadline}
+            onChange={handleDeadline}
+          />
+        ) : (
+          getTime(deadline)
+        )}
       </span>
-      <button className="opacity-40 hover:opacity-100 hover:scale-110 focus:opacity-100" onClick={handleClick}>
+      <button
+        className="opacity-40 hover:opacity-100 hover:scale-110 focus:opacity-100"
+        onClick={handleClick}
+      >
         <img src={EditIcon} alt="edit button" />
       </button>
     </div>
