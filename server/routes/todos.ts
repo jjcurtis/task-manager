b529aaba40037551ed92931ef79e266bee5355d9
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-09-05 14:01:08 -0500

    Refactor todo response into an async function

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -1,34 +1,40 @@
-import { Router } from "express";
-import { readTodos, postTodo, deleteTodo, updateTodoTask, updateTodoDeadlines } from '../../database/client'
+import { Response, Router } from 'express';
+import {
+  readTodos,
+  postTodo,
+  deleteTodo,
+  updateTodoTask,
+  updateTodoDeadlines,
+} from '../../database/client';
 const router = Router();
 
-router.get('/', async (req, res) => {
+async function returnTodos(res: Response) {
   const todos = await readTodos();
-  res.json(todos)
-})
+  res.json(todos);
+}
+
+router.get('/', async (req, res) => {
+  returnTodos(res);
+});
 
 router.post('/', async (req, res) => {
   await postTodo(req.body);
-  const todos = await readTodos();
-  res.json(todos)
-})
+  returnTodos(res);
+});
 
 router.delete('/', async (req, res) => {
   await deleteTodo(req.body);
-  const todos = await readTodos();
-  res.json(todos)
-})
+  returnTodos(res);
+});
 
 router.put('/', async (req, res) => {
   await updateTodoTask(req.body);
-  const todos = await readTodos();
-  res.json(todos);
-})
+  returnTodos(res);
+});
 
 router.put('/deadline', async (req, res) => {
   await updateTodoDeadlines(req.body);
-  const todos = await readTodos();
-  res.json(todos);
-})
+  returnTodos(res);
+});
 
 export default router;
